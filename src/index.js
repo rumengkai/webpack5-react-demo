b9a8fcd84041b3280a142f848cdac26ca4f677d1
林鹤 <he.lin@percent.cn>
2021-10-19 13:02:15 +0800

    添加webapck配置

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 
+import './hello.js'
+import './style.scss'
+import './welcome.ts'
+
+import mainImage from './img.jpg';
+const img = document.getElementById("root")
+img.src = mainImage
+
 console.log('index')
---
+++ src/welcome.ts
@@ -0,0 +1,2 @@
+let str:string = 'welcome'
+console.log(str)
